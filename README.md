24d5b2e8bd055b4571c124de93f1c4f58e6fb1be
kschan <kokseng_92@hotmail.com>
2020-10-26 06:44:34 +0800

    Initial commit
    The web server is capable of serving a static page at localhost:3000

---
+++ README.md
@@ -0,0 +1,9 @@
+# About
+A simple web server built using Rust. It is capable only of serving static files.
+
+## Goals
+1. To learn how to build a web server using Rust
+2. To learn how to serve static files (e.g. HTML, CSS) using the web server
+
+## Reference
+[actix](https://github.com/actix/examples/tree/master/static_index)
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use actix_files::Files;
+use actix_web::{App, HttpServer, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Hello, rust web server!");
+
+    HttpServer::new(|| {
+	App::new()
+	    .configure(app_config)
+    })
+	.bind("127.0.0.1:3000")?
+	.run()
+	.await
+}
+
+fn app_config(config: &mut web::ServiceConfig) {
+    config.service(
+	web::scope("")
+	    .service(Files::new("/", "./static/")
+		     .index_file("index.html")
+	    )
+
+    );
+}
