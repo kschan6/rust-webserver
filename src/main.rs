05b83c9deea63078c55ce6eeebe0ac55f9caf003
kschan <kokseng_92@hotmail.com>
2020-10-31 11:14:46 +0800

    Code refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
 use actix_files::Files;
-use actix_web::{App, HttpServer, web};
+use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Hello, rust web server!");
 
     HttpServer::new(|| {
-	App::new()
-	    .configure(app_config)
+	App::new().service(Files::new("/", "./static/")
+			   .index_file("index.html"))
     })
 	.bind("127.0.0.1:3000")?
 	.run()
 	.await
-}
-
-fn app_config(config: &mut web::ServiceConfig) {
-    config.service(Files::new("/", "./static/")
-		   .index_file("index.html")
-    );
 }
