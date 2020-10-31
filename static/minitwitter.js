136d8f7f07484c6c29713d5344995d2964c0d6e5
kschan <kokseng_92@hotmail.com>
2020-10-31 13:40:56 +0800

    Add JS code that would make a POST request containing the textarea data

---
+++ static/minitwitter.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector('#sec-text button');
+const textarea = document.getElementById('sec-textarea');
+
+btn.addEventListener('click', () => {
+    if (textarea) {
+	sendData({val: textarea.value}); // send a test JSON object
+	clearTextArea();
+    }
+});
+
+function clearTextArea() {
+    if (textarea) {
+	textarea.value = "";
+    }
+}
+
+function sendData(data) {
+    console.log('data:', data);
+
+    fetch('http://localhost:3000/minitwitter', {
+	method: 'POST',
+	headers: {
+	    'Content-Type': 'application/json',
+	},
+	body: JSON.stringify(data), // convert data into a JSON string
+    })
+	.then(res => res.json()) // read the response stream to completion; resolve to a JSON object
+	.then(data => {
+	    console.log('Success:', data);
+	})
+	.catch(err => {
+	    console.log('Error:', err);
+	    alert('Error getting response');
+	});
+}
